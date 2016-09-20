64cbecdb219ee5922f312920d1608b1c0ba5a763
learolland <mail.learolland@gmail.com>
2016-09-20 17:26:35 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,42 @@
+
+/**
+ * @file repl.c
+ * @author François Cayre <cayre@yiking.(null)>
+ * @date Fri Jun 15 18:13:02 2012
+ * @brief Main REPL for SFS.
+ *
+ * Main REPL for SFS.
+ */
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+#include "include/object.h"
+#include "include/read.h"
+#include "include/eval.h"
+#include "include/print.h"
+
+
+
+object nil;
+
+void init_interpreter ( void ) 
+{
+
+    nil      = make_nil();
+
+}
+
+int main ( int argc, char *argv[] ) 
+{
+    char* chaine = malloc(sizeof(chaine));
+    *chaine = "(152)";
+    uint *here = 1;
+    object t = sfs_read_atom(chaine,here);
+    
+    exit( EXIT_SUCCESS );
+}
