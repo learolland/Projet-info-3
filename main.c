d38257e7226f4b4ed658b2134197de1182997b70
learolland <mail.learolland@gmail.com>
2016-09-20 21:11:57 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,6 @@
-
-/**
- * @file repl.c
- * @author François Cayre <cayre@yiking.(null)>
- * @date Fri Jun 15 18:13:02 2012
- * @brief Main REPL for SFS.
- *
- * Main REPL for SFS.
- */
-
-
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
 
 #include "include/object.h"
 #include "include/read.h"
@@ -21,26 +8,16 @@
 #include "include/print.h"
 
 
-
-/*object nil;
-
-void init_interpreter ( void ) 
-{
-
-    nil      = make_nil();
-
-}*/
-
 int main ( int argc, char *argv[] ) 
 {
     char *chaine = malloc(sizeof(*chaine));
     scanf("%s",chaine);
-    /*string tmp = "(152)";
-    strcpy(*chaine,tmp);*/
-    printf("main : %s\n",chaine);
+    printf("main : %s",chaine);
     uint* here = NULL;
     *here = 1;
-    object t = sfs_read_atom(chaine,here);
+    printf(" %d\n",here);
+    object t = NULL;
+    t = sfs_read_atom(chaine,here);
     printf("%d\n",t->type);
     
    /*exit( EXIT_SUCCESS );*/
