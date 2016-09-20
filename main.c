9271edff460588480cd42adf016ca1e1975f4e66
learolland <mail.learolland@gmail.com>
2016-09-20 19:37:56 +0200

    Update main.c

--- main.c
+++ main.c
@@ -39,8 +39,7 @@ int main ( int argc, char *argv[] )
     strcpy(*chaine,tmp);*/
     printf("main : %s\n",chaine);
     uint here = 1;
-    object t =NULL;
-    sfs_read_atom(chaine,&here);
+    object t = sfs_read_atom(chaine,&here);
     printf("%d\n",t->type);
     
    /*exit( EXIT_SUCCESS );*/
