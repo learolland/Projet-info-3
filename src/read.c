b47d40c4b5502acf308ec8b6ce27c973459f806e
learolland <mail.learolland@gmail.com>
2016-09-21 19:43:43 +0200

    Update read.c

--- src/read.c
+++ src/read.c
@@ -296,7 +296,7 @@ uint  sfs_get_sexpr( char *input, FILE *fp ) {
 int test_integer (char* charactere)
 {
 	printf("entree dans test_integer\n");
-	printf("%s\n",charactere[0]);
+	printf("%c\n",charactere[0]);
 	if(charactere[0] ==  '0'|| charactere[0] == '1' || charactere[0] == '2' ||charactere[0] ==  '3' || charactere[0] == '4' ||charactere[0] ==  '5' ||charactere[0] ==  '6' || charactere[0] == '7' || charactere[0] == '8' ||charactere[0] == '9' )
 	return 1;
 	else return 0;
@@ -325,7 +325,7 @@ uint string_to_integer(char *input, uint *here)
 	while(input[*here] != '\0')
 	{
 		printf("boucle %d, input : %c\n",*here,input[*here]);
-		if(test_integer(input[*here])==1)
+		if(test_integer(&input[*here])==1)
 		{
 			strcat(tmp_chaine,&input[*here]);
 			printf("	test_integer = 1, tmp_chaine = %s\n",tmp_chaine);
