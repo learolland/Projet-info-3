0c9290e832d4738fb6b5773e0df3d0d786438f9f
learolland <mail.learolland@gmail.com>
2016-09-22 15:17:40 +0200

    Update main.c

--- main.c
+++ main.c
@@ -12,15 +12,14 @@
 int main ( int argc, char *argv[] ) 
 {
     char *chaine = malloc(sizeof(*chaine));
-    scanf("%s",chaine);
-    printf("main : %s",chaine);
-    
+   /* scanf("%s",chaine);*/
+   
     uint* here = malloc(sizeof(*here));
     *here = 0;
     printf(" %d\n",*here);
     object t = sfs_malloc(sizeof(*t));
-    uint d = string_to_integer(chaine,here);
-    printf("%d\n",d);
+    t = sfs_read_atom(chaine,here);
+    printf("fin\n");
     
    /*exit( EXIT_SUCCESS );*/
 }
